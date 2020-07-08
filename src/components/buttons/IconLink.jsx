b1ee515c6b9855ea5cab474db3eccd645a4f00e3
Sudharsan <g.s.sudharsan99@gmail.com>
2020-07-08 19:13:46 +0530

    Added button component

---
+++ src/components/buttons/IconLink.jsx
@@ -0,0 +1,13 @@
+const IconLink = ({ href, icon, title, className, buttonStyle }) => {
+  return (
+    <button
+      style={buttonStyle}
+      onClick={() => window.open(href, "_blank")}
+      title={title}
+    >
+      <img src={icon} alt={title} className={className} />
+    </button>
+  );
+};
+
+export default IconLink;
