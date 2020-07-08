e1c71bb6f2042c5ab913d63c3d9d898ebc55c600
Sudharsan <g.s.sudharsan99@gmail.com>
2020-07-08 19:06:51 +0530

    Added CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Terminal from "../components/displays/Terminal";
+import CardList from "../components/cards/CardList";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="page" style={{ textAlign: "center" }}>
+      <p className="page-title">Learning OAuth</p>
+      <Terminal userData={"passport.authenticate('facebook')"} selected="All" />
+      <p style={{ fontSize: 28 }}>Popular Strategies</p>
+      <CardList />
+      <div style={{ marginBottom: 20 }} />
+    </div>
+  );
 };
 
 export default Home;
